2071d1e1b7019adb99f3bbc209e363af327b5c52
N.Sanjeev kumar <sanjeevitcit@gmail.com>
2020-03-26 21:46:41 +0530

    gradient tape: update snippet for custom training
    Signed-off-by: N.Sanjeev kumar <sanjeevitcit@gmail.com>

--- gradient_tape.py
+++ gradient_tape.py
@@ -1,7 +1,9 @@
 """
     * GradientTape is released as part of Tensorflow-2.0 to support automatic
     * differentiation. This promotes and eases custom training batches.
-    * Reference: https://www.tensorflow.org/api_docs/python/tf/GradientTape
+    * Reference:
+    *   https://www.tensorflow.org/api_docs/python/tf/GradientTape
+    *   https://www.pyimagesearch.com/2020/03/23/using-tensorflow-and-gradienttape-to-train-a-keras-model/
 """
 import tensorflow as tf
 
@@ -10,3 +12,19 @@ with tf.GradientTape() as g:
   g.watch(x)
   y = x * x
 dy_dx = g.gradient(y, x) # Will compute to 6.0
+
+
+"""
+    * Below is a snippet for custom training using gradient tape.
+    * Call this method on custom loop for n_epochs.
+"""
+
+def train_step(X, y):
+	# keep track of our gradients
+	with tf.GradientTape() as tape:
+		# make a prediction using the model and then calculate the loss
+		prediction = model(X)
+		loss = categorical_crossentropy(y, prediction)
+	# calculate the gradients using our tape and then update the model weights
+	gradients = tape.gradient(loss, model.trainable_variables)
+	opt.apply_gradients(zip(gradients, model.trainable_variables))
